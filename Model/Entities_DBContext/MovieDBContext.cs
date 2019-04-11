ad9ce827fd4d867289eca2385b87064a4bc4ab9e
DESKTOP-2E8CL0V\T Kalubowila <kalubowila99@gmail.com>
2019-04-11 16:09:28 +0530

    Add project files.

---
+++ Model/Entities_DBContext/MovieDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Models.Entities;
+using System.Data.Entity;
+
+namespace Models.Entities_DBContext
+{
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
